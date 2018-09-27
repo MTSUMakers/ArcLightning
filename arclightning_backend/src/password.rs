488510b41315665d359ef896c83324474f9f833d
wchanners <wchanners@yahoo.com>
2018-09-26 21:12:00 -0500

    updated password file and cargo.toml

--- arclightning_backend/src/password.rs
+++ arclightning_backend/src/password.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+pub extern crate blake2_rfc;
+ 
+ pub use password::blake2_rfc::blake2b::blake2b;
+ pub fn check_password(password: String, hash: &[u8]) -> bool {
+    blake2b(64, &[], password.as_bytes()).as_bytes() == hash
 }
